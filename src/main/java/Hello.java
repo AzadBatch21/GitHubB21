040c01f70745413f6630dfbc749fe51d6ba25037
AzadBatch21 <sadikhov.azad@gmail.com>
2021-01-26 14:38:29 -0600

    movie 1 updated hello.java we just new commitsajfnddnkcnd new one

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
         System.out.println("hello");
         System.out.println("new one");
         System.out.println("movie");
+        System.out.println("another commit");
         
         
     }//watching movies
