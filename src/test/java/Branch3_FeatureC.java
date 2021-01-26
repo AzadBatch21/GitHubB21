fb13f42112bf510eb6c39b322c2ee6c5cd8d4a7e
AzadBatch21 <sadikhov.azad@gmail.com>
2021-01-26 13:40:23 -0600

    Notes added and working hard

--- src/test/java/Branch3_FeatureC.java
+++ src/test/java/Branch3_FeatureC.java
@@ -1,5 +1,7 @@
 public class Branch3_FeatureC {
     public static void main(String[] args) {
         System.out.println("Hello Hello Branch3");
+    
+        System.out.println("Notes added");
     }
 }
