b0d59ae5a68a2753fe7cdea046838ce97365d6dd
AzadBatch21 <sadikhov.azad@gmail.com>
2021-01-26 13:23:03 -0600

    started working on new branch notes added class created

--- src/test/java/Branch1_featureACode.java
+++ src/test/java/Branch1_featureACode.java
@@ -2,6 +2,8 @@ public class Branch1_featureACode {
     
     public static void main(String[] args) {
         System.out.println("Awesome work started");
+    
+        System.out.println("B21 is learning more git with review");
     }
     
     
